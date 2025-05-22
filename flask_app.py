c13633d16b64d9874f28e809e275e83ccc0b1eeb
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-05-23 04:48:17 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return "✅ HopperX đang chạy!"
 
-
 @app.route('/healthcheck')
 def healthcheck():
     return "✅ HopperX vẫn sống!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Railway sẽ tự động set PORT
+    app.run(host='0.0.0.0', port=port)
